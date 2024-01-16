093e9e779a11d6959967ef80be271e603ea8733f
StevenYuan666 <ye.yuan3@mail.mcgill.ca>
2024-01-15 21:52:44 -0500

    Update to pass all the auto test

--- code.py
+++ code.py
@@ -1,8 +1,8 @@
-from typing import Union, Iterable, Callable
 import random
+from typing import Callable, Iterable, Union
 
-import torch.nn as nn
 import torch
+import torch.nn as nn
 
 
 def load_datasets(data_directory: str) -> Union[dict, dict]:
@@ -35,7 +35,7 @@ def load_datasets(data_directory: str) -> Union[dict, dict]:
 
 
 def tokenize(
-        text: "list[str]", max_length: int = None, normalize: bool = True
+    text: "list[str]", max_length: int = None, normalize: bool = True
 ) -> "list[list[str]]":
     """
     Tokenize the text into individual words (nested list of string),
@@ -102,7 +102,7 @@ def build_word_counts(token_list: "list[list[str]]") -> "dict[str, int]":
 
 
 def build_index_map(
-        word_counts: "dict[str, int]", max_words: int = None
+    word_counts: "dict[str, int]", max_words: int = None
 ) -> "dict[str, int]":
     """
     Builds an index map that converts a word into an integer that can be
@@ -126,7 +126,7 @@ def build_index_map(
 
     sorted_counts = sorted(word_counts.items(), key=lambda item: item[1], reverse=True)
     if max_words:
-        sorted_counts = sorted_counts[:max_words - 1]
+        sorted_counts = sorted_counts[: max_words - 1]
 
     sorted_words = ["[PAD]"] + [item[0] for item in sorted_counts]
 
@@ -134,7 +134,7 @@ def build_index_map(
 
 
 def tokens_to_ix(
-        tokens: "list[list[str]]", index_map: "dict[str, int]"
+    tokens: "list[list[str]]", index_map: "dict[str, int]"
 ) -> "list[list[int]]":
     """
     Converts a nested list of tokens to a nested list of indices using
@@ -163,47 +163,35 @@ def tokens_to_ix(
 
 ### 1.1 Batching, shuffling, iteration
 def build_loader(
-        data_dict: dict, batch_size: int = 64, shuffle: bool = False
+    data_dict: dict, batch_size: int = 64, shuffle: bool = False
 ) -> Callable[[], Iterable[dict]]:
     # TODO: Your code here
 
     # split the data into premise, hypothesis, and label
-    premise = data_dict["premise"]
-    hypothesis = data_dict["hypothesis"]
-    label = data_dict["label"]
+    keys = list(data_dict.keys())
+    length = len(data_dict[keys[0]])
 
-    # create a list of indices
-    indices = list(range(len(premise)))
-
-    # shuffle the data if needed
-    if shuffle:
-        random.shuffle(indices)
-        premise = [premise[ix] for ix in indices]
-        hypothesis = [hypothesis[ix] for ix in indices]
-        label = [label[ix] for ix in indices]
-
-    # Assume each key in data_dict has the same length
-    num_batches = len(premise) // batch_size
-    remainder = len(premise) % batch_size
-
-    # create a generator that returns a batch of data
     def loader():
-        # TODO: Your code here
-        count = 0
-        while count < num_batches:
+        indices = list(range(length))
+        if shuffle:
+            random.shuffle(indices)
+
+        num_batches = length // batch_size
+        for count in range(num_batches):
             batch = {
-                "premise": premise[count * batch_size: (count + 1) * batch_size],
-                "hypothesis": hypothesis[count * batch_size: (count + 1) * batch_size],
-                "label": torch.tensor(label[count * batch_size: (count + 1) * batch_size], dtype=torch.float)
+                key: [
+                    data_dict[key][ix]
+                    for ix in indices[count * batch_size : (count + 1) * batch_size]
+                ]
+                for key in keys
             }
-            count += 1
             yield batch
-        # when count == num_batches, return the remaining data
-        if remainder != 0:
+
+        remainder = length % batch_size
+        if remainder:
             batch = {
-                "premise": premise[count * batch_size:],
-                "hypothesis": hypothesis[count * batch_size:],
-                "label": torch.tensor(label[count * batch_size:], dtype=torch.float)
+                key: [data_dict[key][ix] for ix in indices[num_batches * batch_size :]]
+                for key in keys
             }
             yield batch
 
@@ -218,9 +206,9 @@ def convert_to_tensors(text_indices: "list[list[int]]") -> torch.Tensor:
     max_length = max([len(text) for text in text_indices])
 
     # convert the text indices into a tensor
-    tensor = torch.zeros((len(text_indices), max_length), dtype=torch.long)
+    tensor = torch.zeros((len(text_indices), max_length), dtype=torch.int32)
     for i, text in enumerate(text_indices):
-        tensor[i, :len(text)] = torch.tensor(text)
+        tensor[i, : len(text)] = torch.tensor(text, dtype=torch.int32)
 
     return tensor
 
@@ -243,10 +231,11 @@ class PooledLogisticRegression(nn.Module):
         # Initialize the embedding layer
         self.embedding = embedding
         # Initialize the logistic regression layer
-        self.layer_pred = nn.Linear(2 * self.embedding.embedding_dim, 1)  # (2 * hidden_size, 1)
+        self.layer_pred = nn.Linear(
+            2 * self.embedding.embedding_dim, 1
+        )  # (2 * hidden_size, 1)
         # Initialize the sigmoid layer
         self.sigmoid = nn.Sigmoid()
-
 
     # DO NOT CHANGE THE SECTION BELOW! ###########################
     # # This is to force you to initialize certain things in __init__
@@ -275,7 +264,9 @@ class PooledLogisticRegression(nn.Module):
         premise_pool = max_pool(premise_emb)  # (batch_size, hidden_size)
         hypothesis_pool = max_pool(hypothesis_emb)  # (batch_size, hidden_size)
         # Concatenate the premise and hypothesis
-        concat = torch.cat((premise_pool, hypothesis_pool), dim=1)  # (batch_size, 2 * hidden_size)
+        concat = torch.cat(
+            (premise_pool, hypothesis_pool), dim=1
+        )  # (batch_size, 2 * hidden_size)
         # Get the prediction
         pred = layer_pred(concat)  # (batch_size, 1)
         # Get the probability
@@ -286,13 +277,14 @@ class PooledLogisticRegression(nn.Module):
         return prob
 
 
-
 ### 2.2 Choose an optimizer and a loss function
 def assign_optimizer(model: nn.Module, **kwargs) -> torch.optim.Optimizer:
     # TODO: Your code here
 
     # Initialize the optimizer, we use Adam here, not SGD
-    optimizer = torch.optim.Adam(model.parameters(), **kwargs)  # (model.parameters(), lr=3e-4, weight_decay=1e-2)
+    optimizer = torch.optim.Adam(
+        model.parameters(), **kwargs
+    )  # (model.parameters(), lr=3e-4, weight_decay=1e-2)
     return optimizer
 
 
@@ -311,7 +303,7 @@ def forward_pass(model: nn.Module, batch: dict, device="cpu"):
 
     # Get the premise, hypothesis, and label
     premise = batch["premise"]  # (batch_size, seq_len)
-    hypothesis = batch["hypothesis"] # (batch_size, seq_len)
+    hypothesis = batch["hypothesis"]  # (batch_size, seq_len)
 
     # Convert the premise and hypothesis into tensors
     premise = convert_to_tensors(premise).to(device)  # (batch_size, seq_len)
@@ -325,7 +317,7 @@ def forward_pass(model: nn.Module, batch: dict, device="cpu"):
 
 
 def backward_pass(
-        optimizer: torch.optim.Optimizer, y: torch.Tensor, y_pred: torch.Tensor
+    optimizer: torch.optim.Optimizer, y: torch.Tensor, y_pred: torch.Tensor
 ) -> torch.Tensor:
     # TODO: Your code here
 
@@ -376,7 +368,7 @@ def f1_score(y: torch.Tensor, y_pred: torch.Tensor, threshold=0.5) -> torch.Tens
 
 ### 2.5 Train loop
 def eval_run(
-        model: nn.Module, loader: Callable[[], Iterable[dict]], device: str = "cpu"
+    model: nn.Module, loader: Callable[[], Iterable[dict]], device: str = "cpu"
 ):
     # TODO: Your code here
 
@@ -385,7 +377,7 @@ def eval_run(
     y_pred = []
     for batch in loader():
         # Get the label
-        label = batch["label"].to(device)
+        label = torch.tensor(batch["label"], dtype=torch.float).to(device)
         # Get the prediction
         pred = forward_pass(model, batch, device)
         # Collect the label
@@ -403,12 +395,12 @@ def eval_run(
 
 
 def train_loop(
-        model: nn.Module,
-        train_loader,
-        valid_loader,
-        optimizer,
-        n_epochs: int = 3,
-        device: str = "cpu",
+    model: nn.Module,
+    train_loader,
+    valid_loader,
+    optimizer,
+    n_epochs: int = 3,
+    device: str = "cpu",
 ):
     # TODO: Your code here
 
@@ -431,7 +423,7 @@ def train_loop(
             # Call forward pass
             y_pred = forward_pass(model, batch, device)
             # Call backward pass
-            y_label = batch["label"].to(device)
+            y_label = torch.tensor(batch["label"], dtype=torch.float).to(device)
             loss = backward_pass(optimizer, y_label, y_pred)
             # Accumulate the loss
             train_loss += loss.item()
@@ -471,7 +463,7 @@ class ShallowNeuralNetwork(nn.Module):
         # Initialize the embedding layer
         self.embedding = embedding
         # Initialize the logistic regression layer
-        self.layer_pred = nn.Linear(self.embedding.embedding_dim, 1)
+        self.layer_pred = nn.Linear(hidden_size, 1)
         # Initialize the sigmoid layer
         self.sigmoid = nn.Sigmoid()
         # Initialize the activation function
@@ -514,7 +506,9 @@ class ShallowNeuralNetwork(nn.Module):
         premise_pool = max_pool(premise_emb)  # (batch_size, hidden_size)
         hypothesis_pool = max_pool(hypothesis_emb)  # (batch_size, hidden_size)
         # Concatenate the premise and hypothesis
-        concat = torch.cat((premise_pool, hypothesis_pool), dim=1)  # (batch_size, 2 * hidden_size)
+        concat = torch.cat(
+            (premise_pool, hypothesis_pool), dim=1
+        )  # (batch_size, 2 * hidden_size)
         # Apply the feedforward layer
         ff = ff_layer(concat)  # (batch_size, hidden_size)
         # Apply the activation function
@@ -539,13 +533,16 @@ class DeepNeuralNetwork(nn.Module):
         # Initialize the embedding layer
         self.embedding = embedding
         # Initialize the logistic regression layer
-        self.layer_pred = nn.Linear(2 * self.embedding.embedding_dim, 1)
+        self.layer_pred = nn.Linear(hidden_size, 1)
         # Initialize the sigmoid layer
         self.sigmoid = nn.Sigmoid()
         # Initialize the activation function
         self.activation = nn.ReLU()
         # Initialize the feedforward layers
-        self.ff_layers = nn.ModuleList([nn.Linear(2 * self.embedding.embedding_dim, 2 * hidden_size) for _ in range(num_layers)])
+        self.ff_layers = nn.ModuleList(
+            [nn.Linear(2 * self.embedding.embedding_dim, hidden_size)]
+            + [nn.Linear(hidden_size, hidden_size) for _ in range(num_layers - 1)]
+        )
 
     # DO NOT CHANGE THE SECTION BELOW! ###########################
     # # This is to force you to initialize certain things in __init__
@@ -582,7 +579,9 @@ class DeepNeuralNetwork(nn.Module):
         premise_pool = max_pool(premise_emb)  # (batch_size, hidden_size)
         hypothesis_pool = max_pool(hypothesis_emb)  # (batch_size, hidden_size)
         # Concatenate the premise and hypothesis
-        concat = torch.cat((premise_pool, hypothesis_pool), dim=1)  # (batch_size, 2 * hidden_size)
+        concat = torch.cat(
+            (premise_pool, hypothesis_pool), dim=1
+        )  # (batch_size, 2 * hidden_size)
         # Apply the feedforward layers
         for ff_layer in ff_layers:
             concat = ff_layer(concat)
@@ -633,13 +632,13 @@ if __name__ == "__main__":
     train_indices = {
         "label": train_raw["label"],
         "premise": tokens_to_ix(train_tokens["premise"], index_map),
-        "hypothesis": tokens_to_ix(train_tokens["hypothesis"], index_map)
+        "hypothesis": tokens_to_ix(train_tokens["hypothesis"], index_map),
     }
 
     valid_indices = {
         "label": valid_raw["label"],
         "premise": tokens_to_ix(valid_tokens["premise"], index_map),
-        "hypothesis": tokens_to_ix(valid_tokens["hypothesis"], index_map)
+        "hypothesis": tokens_to_ix(valid_tokens["hypothesis"], index_map),
     }
 
     # 1.1
@@ -666,7 +665,6 @@ if __name__ == "__main__":
 
     # 2.5
     n_epochs = 5
-
 
     embedding = torch.nn.Embedding(10000, 512)  # max_words, embedding_dim
     model = PooledLogisticRegression(embedding)
